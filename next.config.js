d16e6d25c769694f628ec22852a856661bc40d4a
Haripriya2408 <haripriyaveluchamy@gmail.com>
2025-01-18 13:28:38 +0530

    con

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true // Add this line to prevent ESLint from failing the build
-  }
+  output: 'standalone',
 }
 
 module.exports = nextConfig
